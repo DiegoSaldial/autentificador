5365a21afb766ffe9b80a59eba75ada214eac666
Diegotes <diego.saldias@esam.edu.bo>
2024-04-14 18:05:58 -0400

    ok

--- web/src/shared/session.js
+++ web/src/shared/session.js
@@ -1,30 +1,33 @@
-import {useLoginStore} from 'stores/login-store'
-import { jwtDecode } from 'jwt-decode'
-import { Notify } from 'quasar'
+import { useLoginStore } from 'stores/login-store';
+import { jwtDecode } from 'jwt-decode';
+import { Notify } from 'quasar';
 
 export default {
   name: 'MyLayout',
-  components:{},
+  components: {},
 
-  setup () {
+  setup() {
     const store = useLoginStore();
 
     const checkClickSession = () => {
       const refreshToken = store.getRefreshToken.value || '';
-      if(!refreshToken) return;
+      if (!refreshToken) return;
       const decodedToken = jwtDecode(refreshToken);
       const currentTime = Date.now() / 1000;
       const expirationTime = decodedToken.exp || 0;
       const timeRemaining = expirationTime - currentTime;
-      console.log( '>>>>>>>>>>>>>>>>', timeRemaining);
-      if(timeRemaining < 0 ){
+      // console.log( '>>>>>>>>>>>>>>>>', timeRemaining);
+      if (timeRemaining < 0) {
         store.setToken();
-        Notify.create({message:'Session expirada, por favor vuelva a ingresar.',color:'red'})
+        Notify.create({
+          message: 'Session expirada, por favor vuelva a ingresar.',
+          color: 'red',
+        });
       }
-    }
+    };
 
     return {
       checkClickSession,
-    }
-  }
-}
+    };
+  },
+};
